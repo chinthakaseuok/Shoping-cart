eb0c2afafeb67a1d99dd33ea08ca6a00fd0b65cb
chinthakaseuok <kgcchathuranga97@gmail.com>
2021-03-03 02:39:05 +0530

    import Dropdown react bootstrap and done with Navigating bar Ux

--- src/ClientApp.tsx
+++ src/ClientApp.tsx
@@ -2,11 +2,13 @@ import React from "react";
 import {Container} from "react-bootstrap";
 import Footer from "./component/footer/Footer";
 import Header from "./component/header/Header";
+import Content from "./component/content/Content";
 
 const ClientApp:React.FC = () =>{
     return(
         <Container fluid >
             <Header/>
+            <Content/>
             <Footer/>
         </Container>
     );
---
+++ src/component/content/Content.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import NavigationBar from "./NavigationBar";
+
+const Content : React.FC = () =>{
+    return(
+        <NavigationBar/>
+    );
+};
+export default Content;
