ef48fbaedd22cb44ce4759afd36a595138652fd3
chinthakaseuok <kgcchathuranga97@gmail.com>
2021-03-06 12:36:10 +0530

    insert banner and banner content. insert new two font family "Crimson Text" and "Akaya Telivigala" for banner content.

--- src/component/content/Content.tsx
+++ src/component/content/Content.tsx
@@ -1,9 +1,15 @@
 import React from "react";
 import NavigationBar from "./NavigationBar";
+import Home from "./Home/Home";
 
 const Content : React.FC = () =>{
     return(
-        <NavigationBar/>
+        <React.Fragment>
+            <NavigationBar/>
+            <Home/>
+        </React.Fragment>
+
+
     );
 };
 export default Content;
---
+++ src/component/content/Home/Home.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import {Row} from "react-bootstrap";
+import Banner from "./Banner";
+
+const Home: React.FC = () =>{
+    return(
+        <Row>
+            <Banner/>
+        </Row>
+    );
+};
+export default Home;
