bec07c0a1386dcb7c61929e159d9f219f4253b1e
chinthakaseuok <kgcchathuranga97@gmail.com>
2021-03-11 16:21:40 +0530

    insert search bar to home.

--- src/component/content/Home/Home.tsx
+++ src/component/content/Home/Home.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import {Row} from "react-bootstrap";
 import Banner from "./Banner";
+import SearchBar from "./SearchBar";
 
 const Home: React.FC = () =>{
     return(
         <Row>
             <Banner/>
+            <SearchBar/>
         </Row>
     );
 };
