b38dabaf472bcc6d4b26c8093bf2faf3c83b0d52
chinthakaseuok <kgcchathuranga97@gmail.com>
2021-03-11 18:54:24 +0530

    insert category list and align center.

---
+++ src/component/content/Home/CategoryView.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import {Col, Row} from "react-bootstrap";
+import CategoryImage from '../../../assets/images/store-category-1.jpg';
+
+const CategoryView: React.FC = () => {
+
+    return (
+        <Col xs={4} md={4} lg={2} className='px-4 mb-3'>
+            <Row className='category-view text-center'>
+                <Col xs={12} className='p-2'>
+                    <img className='category-image m-0 p-0' src={CategoryImage} alt={CategoryImage}/>
+                    <br/>
+                    <span>Grocery</span>
+                </Col>
+            </Row>
+        </Col>
+
+    );
+
+};
+
+export default CategoryView;
--- src/component/content/Home/Home.tsx
+++ src/component/content/Home/Home.tsx
@@ -2,12 +2,16 @@ import React from "react";
 import {Row} from "react-bootstrap";
 import Banner from "./Banner";
 import SearchBar from "./SearchBar";
+import CategoryList from "./CategoryList";
+import CategoryHorizontalList from "./CategoryHorizontalList";
 
 const Home: React.FC = () =>{
     return(
         <Row>
             <Banner/>
             <SearchBar/>
+            <CategoryList/>
+            <CategoryHorizontalList/>
         </Row>
     );
 };
